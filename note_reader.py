ed692fccf67c570ff8e597bf039fecd872bb252e
ihpar <ismail.hakki.parlak@gmail.com>
2023-08-03 15:47:53 +0300

    filters out uncommon pitch classes

--- note_reader.py
+++ note_reader.py
@@ -1,9 +1,13 @@
+from collections import Counter
+
 note_line_codes = ["1", "4", "7", "8", "9",
                    "10", "11", "12", "23", "24", "28", "44"]
 
 
-def get_notes(file_list, as_pitch_classes=False, makam=None):
+def get_notes(file_list, as_pitch_classes=False, return_counts=False, makam=None):
     all_notes = []
+    counts = Counter()
+
     for file_path in file_list:
         if makam and makam + "--" not in file_path:
             continue
@@ -15,10 +19,17 @@ def get_notes(file_list, as_pitch_classes=False, makam=None):
                 tokens = line.split("\t")
                 if tokens[1] in note_line_codes:
                     note = tokens[3]
+                    if note == "Es":
+                        continue
                     if as_pitch_classes and note[1].isdigit():
-                        notes.append(note[0] + note[2:])
+                        pitch_class = note[0] + note[2:]
+                        notes.append(pitch_class)
                     else:
                         notes.append(note)
             all_notes.append(notes)
+            counts.update(notes)
+
+    if return_counts:
+        return all_notes, counts
 
     return all_notes
