d6e2e33dd79db30ed2459c881863b21eaf8d60be
ihpar <ismail.hakki.parlak@gmail.com>
2023-08-08 15:23:25 +0300

    adds symbol conversion: defaults to sharp

--- note_reader.py
+++ note_reader.py
@@ -3,8 +3,68 @@ from collections import Counter
 note_line_codes = ["1", "4", "7", "8", "9",
                    "10", "11", "12", "23", "24", "28", "44"]
 
+pitches = ["G", "A", "B", "C", "D", "E", "F"]
+kommas = [9, 9, 4, 9, 9, 4, 9]
 
-def get_notes(file_list, as_pitch_classes=False, return_counts=False, makam=None):
+
+def convert_to_sharp(note):
+    """
+    Converts notes with accidental symbols to all sharp rep.'s
+
+    G --> G, 
+    Bb9 --> A, 
+    Eb4 --> D#5, 
+    Bb1 --> A#8, 
+    Bb13 --> G#5, 
+    Gb5 --> F#4, 
+    F#4 --> F#4, 
+    E#4 --> F, 
+    E#5 --> F#1, 
+    E#13 --> G
+    """
+
+    if len(note) == 1:
+        return note
+
+    accidental = note[1]
+    if accidental not in ["#", "b"]:
+        raise ValueError("Accidental symbol unrecognised!")
+
+    root = note[0]
+    idx = pitches.index(root)
+    komma = int(note[2:])
+    next_root, result = None, None
+    remaining_komma = komma
+
+    if accidental == "b":
+        while remaining_komma > 0:
+            idx = (idx - 1) % 7
+            next_root = pitches[idx]
+            remaining_komma -= kommas[idx]
+
+        result = next_root
+        if remaining_komma < 0:
+            result += "#" + str(-1 * remaining_komma)
+
+    elif accidental == "#":
+        while remaining_komma > 0:
+            remaining_komma -= kommas[idx]
+            idx = (idx + 1) % 7
+            next_root = pitches[idx]
+
+        if remaining_komma < 0:
+            idx = (idx - 1) % 7
+            next_root = pitches[idx]
+            remaining_komma += kommas[idx]
+
+        result = next_root
+        if remaining_komma != 0:
+            result += "#" + str(remaining_komma)
+
+    return result
+
+
+def get_notes_as_pitch_classes(file_list, makam=None):
     all_notes = []
     counts = Counter()
 
@@ -19,17 +79,13 @@ def get_notes(file_list, as_pitch_classes=False, return_counts=False, makam=None
                 tokens = line.split("\t")
                 if tokens[1] in note_line_codes:
                     note = tokens[3]
-                    if note == "Es":
+                    if note == "Es" or (not note[1].isdigit()):
                         continue
-                    if as_pitch_classes and note[1].isdigit():
-                        pitch_class = note[0] + note[2:]
-                        notes.append(pitch_class)
-                    else:
-                        notes.append(note)
+
+                    pitch_class = convert_to_sharp(note[0] + note[2:])
+                    notes.append(pitch_class)
+
             all_notes.append(notes)
             counts.update(notes)
 
-    if return_counts:
-        return all_notes, counts
-
-    return all_notes
+    return all_notes, counts
